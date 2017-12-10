5dd5e80e336ab07a925c6653a057436a61219a49
Kxoe <kxoethecat@gmail.com>
2017-12-09 22:23:22 -0700

    Implemented basic Mouse and Keyboard

--- source/Mouse.d
+++ source/Mouse.d
@@ -4,11 +4,15 @@ import d2d.EventHandler;
 import d2d.InputSource;
 import d2d.sdl2;
 
+import std.algorithm;
+import std.datetime;
+
 /**
  * The mouse input source which acculmulates mouse information
  */
 class Mouse : InputSource, EventHandler {
 
+    Pressable[uint] allButtons;    ///All of the buttons on the mouse
     private iPoint screenLocation; ///Location of the mouse within the entire screen
     private iPoint windowLocation; ///Location of the mouse within the window
 
@@ -18,12 +22,31 @@ class Mouse : InputSource, EventHandler {
     override @property Pressable[] allPressables() {
         return null;
     }
+    
+    /**
+     * Returns the location of the mouse
+     */
+    @property iPoint location() {
+        int x, y;
+		SDL_GetMouseState(&x, &y);
+        return new iPoint(x, y);
+    }
 
     /**
      * Acculmulates all of the mouse events and updates stored pressables accordingly
      */
     override void handleEvent(SDL_Event event) {
-
+        if(event.type == SDL_MOUSEBUTTONDOWN){
+            if(!this.allButtons.keys.canFind(event.button.button)){
+                this.allButtons[event.button.button] = Pressable();
+            }
+            this.allButtons[event.button.button].lastPressed = Clock.currTime();
+        }else if(event.type == SDL_KEYUP){
+            if(!this.allButtons.keys.canFind(event.button.button)){
+                this.allButtons[event.button.button] = Pressable();
+            }
+            this.allButtons[event.button.button].lastReleased = Clock.currTime();
+        }
     }
 
 }
