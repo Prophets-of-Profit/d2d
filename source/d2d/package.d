11345ac94f7431089756e74a8e9347f799ae517d
Kxoe <kxoethecat@gmail.com>
2017-12-15 12:09:39 -0700

    Added package file which imports all d2d modules

---
+++ source/d2d/package.d
@@ -0,0 +1,15 @@
+module d2d;
+
+/**
+ * This file imports all of the modules of d2d
+ */
+
+public import d2d.Component;
+public import d2d.Display;
+public import d2d.EventHandler;
+public import d2d.InputSource;
+public import d2d.Keyboard;
+public import d2d.Mouse;
+public import d2d.Screen;
+public import d2d.Utility;
+public import d2d.sdl2;
